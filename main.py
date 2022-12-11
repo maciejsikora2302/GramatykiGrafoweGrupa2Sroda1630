6c08ebb009273a03b827c4e312ead28500395a71
Maciej Sikora <maciejsikora2302@gmail.com>
2022-12-11 16:55:51 +0100

    p2 production implemented

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ Let's start with generating P1.
 import networkx as nx
 import matplotlib.pyplot as plt
 from pprint import pprint as pp
-from typing import Union, List, Tuple, Dict, Any, Optional
+import typing
 from copy import copy
 
 class Attribute:
@@ -48,6 +48,7 @@ def draw(graph: nx.Graph, level: int = None, filename: str='test_draw.png') -> N
     label_attr = nx.get_node_attributes(graph, Attribute.LABEL)
     xs = nx.get_node_attributes(graph, Attribute.X)
     ys = nx.get_node_attributes(graph, Attribute.Y)
+    levels = nx.get_node_attributes(graph, Attribute.LEVEL)
 
     red_labels = ['El', 'el']
     blue_labels = ['E', 'e']
@@ -65,7 +66,7 @@ def draw(graph: nx.Graph, level: int = None, filename: str='test_draw.png') -> N
             node_colors.append('white')
 
 
-    labels = { node: f'{label_attr[node], (xs[node], ys[node])}' for node in graph.nodes()}
+    labels = { node: f'{label_attr[node], (xs[node], ys[node]), levels[node]}' for node in graph.nodes()}
 
     nx.draw_networkx_nodes(graph, pos, node_size=2000, node_color=node_colors, edgecolors='black', linewidths=5, alpha=0.5)
     nx.draw_networkx_edges(graph, pos, width=5, alpha=0.3)
@@ -90,38 +91,42 @@ def node_comparator(found_node, searched_node) -> bool:
     return found_node[Attribute.LABEL] == searched_node[Attribute.LABEL]
     # return found_node[Attribute.LEVEL] == searched_node[Attribute.LEVEL] and found_node[Attribute.LABEL] == searched_node[Attribute.LABEL]
 
+def node_comparator_factory(level: int) -> typing.Callable:
+    def node_comparator(found_node, searched_node) -> bool:
+        return found_node[Attribute.LEVEL] == level and found_node[Attribute.LABEL] == searched_node[Attribute.LABEL]
 
-def find_isomporphic(graph: nx.Graph, left_side_graph: nx.Graph) -> dict:
-    # for isomorphic_graph in nx.algorithms.isomorphism.GraphMatcher(graph, left_side_graph, node_match=node_comparator).subgraph_isomorphisms_iter():
-    #     return isomorphic_graph
-    # return None
+    return node_comparator
+
+
+def find_isomporphic(graph: nx.Graph, left_side_graph: nx.Graph, level: int) -> dict:
     isomorphic_graphs = []
-    for isomorphic_graph in nx.algorithms.isomorphism.GraphMatcher(graph, left_side_graph, node_match=node_comparator).subgraph_isomorphisms_iter():
+    graph_matcher = nx.algorithms.isomorphism.GraphMatcher(graph, left_side_graph, node_match=node_comparator_factory(level))
+    for isomorphic_graph in graph_matcher.subgraph_isomorphisms_iter():
         isomorphic_graphs.append(isomorphic_graph)
     return isomorphic_graphs
 
-def find_isomporphic_wrapper(graph: nx.Graph, left_side_graph: nx.Graph) -> dict:
-    return find_isomporphic(graph, left_side_graph)
+def find_isomporphic_wrapper(graph: nx.Graph, left_side_graph: nx.Graph, level: int) -> dict:
+    return find_isomporphic(graph, left_side_graph, level)
 
-def p1(graph: nx.Graph):
+def p1(graph: nx.Graph, level: int):
     left_production_side_graph = nx.Graph()
     parent_tmp_node_number = 8
 
     left_production_side_graph.add_nodes_from([
-        (parent_tmp_node_number, dict(label='El', x=1.0, y=1.0, level=0))
+        (parent_tmp_node_number, dict(label='El', x=None, y=None, level=0))
     ])
-    pp(f"Initial left graph: {type(left_production_side_graph)}")
+    # pp(f"Initial left graph: {type(left_production_side_graph)}")
 
     n = len(graph.nodes)
 
-    right_side_parent_node = (parent_tmp_node_number, dict(label='el', x=1.0, y=1.0, level=0)) # only new node is here!
+    right_side_parent_node = (parent_tmp_node_number, dict(label='el', x=None, y=None, level=level)) # only new node is here!
     right_side_nodes_new = [
-        (1, dict(label='I', x=2.0, y=1.0, level=1)), # others have to be generated!
-        (2, dict(label='I', x=3.0, y=1.0, level=1)),
-        (3, dict(label='E', x=4.0, y=1.0, level=1)),
-        (4, dict(label='E', x=5.0, y=1.0, level=1)),
-        (5, dict(label='E', x=5.0, y=1.0, level=1)),
-        (6, dict(label='E', x=5.0, y=1.0, level=1))
+        (1, dict(label='I', x=None, y=None, level=1)), # others have to be generated!
+        (2, dict(label='I', x=None, y=None, level=1)),
+        (3, dict(label='E', x=4.0, y=4.0, level=1)),
+        (4, dict(label='E', x=5.0, y=5.0, level=1)),
+        (5, dict(label='E', x=6.0, y=6.0, level=1)),
+        (6, dict(label='E', x=7.0, y=7.0, level=1))
     ]
 
     right_side_nodes = [right_side_parent_node] + right_side_nodes_new
@@ -134,17 +139,79 @@ def p1(graph: nx.Graph):
     right_production_side.add_nodes_from(right_side_nodes)
     right_production_side.add_edges_from(right_side_edges)
 
-    isomorphic_mapping = find_isomporphic_wrapper(graph, left_production_side_graph)[0]
+    isomorphic_mapping = find_isomporphic_wrapper(graph, left_production_side_graph, level)[0]
+    add_to_graph(graph, isomorphic_mapping, right_side_parent_node, right_side_nodes_new, right_side_edges)
 
-    # create reverse mapping
+def p2(graph: nx.Graph, level: int) -> None:
+    left_production_side_graph = nx.Graph()
+
+    parent_tmp_node_number = 8
+    left_production_side_graph.add_nodes_from([
+        (parent_tmp_node_number, dict(label='I', x=None, y=None, level=None)),
+        (1, dict(label='E', x=None, y=None, level=None)),
+        (2, dict(label='E', x=None, y=None, level=None)),
+        (3, dict(label='E', x=None, y=None, level=None))
+    ])
+    left_production_side_graph.add_edges_from([
+        (parent_tmp_node_number, 1),
+        (parent_tmp_node_number, 2),
+        (parent_tmp_node_number, 3),
+        (1, 2),
+        (1, 3),
+        (2, 3)
+    ])
+
+    isomorphic_mapping = find_isomporphic_wrapper(graph, left_production_side_graph, level=level)
+    isomorphic_mapping = isomorphic_mapping[0]
+
+    
+    X = []
+    Y = []
+    for node in isomorphic_mapping.keys():
+        if graph.nodes[node][Attribute.LABEL] == 'E':
+            X.append(graph.nodes[node][Attribute.X])
+            Y.append(graph.nodes[node][Attribute.Y])
+        
+
+    # pp(f"X: {X}")
+    # pp(f"Y: {Y}")
+
+    right_side_parent_node = (parent_tmp_node_number, dict(label='i', x=None, y=None, level=None))
+    right_side_nodes_new = [
+        (1, dict(label='I', x=None, y=None, level=None)), # others have to be generated!
+        (2, dict(label='I', x=None, y=None, level=None)),
+        (3, dict(label='E', x=X[0], y=Y[0], level=None)),
+        (4, dict(label='E', x=X[1], y=Y[1], level=None)),
+        (5, dict(label='E', x=X[2], y=Y[2], level=None)),
+        (6, dict(label='E', x=(X[0]+X[2])/2, y=(Y[0]+Y[2])/2, level=None))
+    ]
+
+    right_side_nodes = [right_side_parent_node] + right_side_nodes_new
+
+    right_side_edges = [
+        (3,4), (3,6), (3,1), (4,5), (4,6), (4,1), (4,2), (5,2), (5,6), (6,1), (6,2), (parent_tmp_node_number,1), (parent_tmp_node_number,2),
+    ]
+
+    right_production_side = nx.Graph()
+    right_production_side.add_nodes_from(right_side_nodes)
+    right_production_side.add_edges_from(right_side_edges)
+
+
+    add_to_graph(graph, isomorphic_mapping, right_side_parent_node, right_side_nodes_new, right_side_edges)
+
+def add_to_graph(
+    graph: nx.Graph, 
+    isomorphic_mapping: dict, 
+    right_side_parent_node: tuple, 
+    right_side_nodes_new: list, 
+    right_side_edges: list
+    ):
+    parent_tmp_node_number = right_side_parent_node[0]
+
     isomorphic_mapping_reverse = {v: k for k, v in isomorphic_mapping.items()}
 
-    pp(f"Isomorphic mapping: {isomorphic_mapping}")
-    pp(f"Isomorphic mapping reverse: {isomorphic_mapping_reverse}")
-
-    # right_side_existing_node_mapped = isomorphic_mapping[right_side_nodes_existing[0]]
-
-    right_side_nodes_mapping = { node[0]: node[0]+n for node in right_side_nodes_new } # define a dictionay mapping old node number (based on right_side_nodes ) => graph node.
+    n = len(graph.nodes)
+    right_side_nodes_mapping = { node[0]: node[0] + n for node in right_side_nodes_new } # define a dictionay mapping old node number (based on right_side_nodes ) => graph node.
     right_side_nodes_mapping[parent_tmp_node_number] = isomorphic_mapping_reverse[parent_tmp_node_number]
         
 
@@ -162,73 +229,23 @@ def p1(graph: nx.Graph):
 
     graph.nodes[isomorphic_mapping_reverse[parent_tmp_node_number]][Attribute.LABEL] = right_side_parent_node[1][Attribute.LABEL]
 
-
     graph.add_nodes_from(right_side_nodes_mapped)
     graph.add_edges_from(right_side_edges_mapped)
 
-    # pp(f"Isomorphic graph: {type(isomorphic_graph)}")
-    pp(f"Isomorphic graph size: {len(graph)}")
-    pp(f"Isomorphic graph: {graph}")
-
-    # print all labels
-    for node in graph.nodes:
-        print(graph.nodes[node][Attribute.LABEL])
-
-
-def p2(graph: nx.Graph) -> None:
-    left_production_side_graph = nx.Graph()
-
-    parent_tmp_node_number = 8
-    left_production_side_graph.add_nodes_from([
-        (parent_tmp_node_number, dict(label='I', x=None, y=None, level=None))
-        (1, dict(label='E', x=None, y=None, level=None)),
-        (2, dict(label='E', x=None, y=None, level=None)),
-        (3, dict(label='E', x=None, y=None, level=None)),
-    ])
-    left_production_side_graph.add_edges_from([
-        (parent_tmp_node_number, 1),
-        (parent_tmp_node_number, 2),
-        (parent_tmp_node_number, 3),
-        (1, 2),
-        (1, 3),
-        (2, 3)
-    ])
-
-    pp(f"Initial left graph: {left_production_side_graph}")
-
-    draw(left_production_side_graph, 'left_production_side_graph.png')
-    # n = len(graph.nodes)
-
-    # right_side_parent_node = (parent_tmp_node_number, dict(label='el', x=1.0, y=1.0, level=0)) # only new node is here!
-    # right_side_nodes_new = [
-    #     (1, dict(label='I', x=None, y=None, level=None)), # others have to be generated!
-    #     (2, dict(label='I', x=None, y=None, level=None)),
-    #     (3, dict(label='E', x=None, y=None, level=None)),
-    #     (4, dict(label='E', x=None, y=None, level=None)),
-    #     (5, dict(label='E', x=None, y=None, level=None)),
-    #     (6, dict(label='E', x=None, y=None, level=None))
-    # ]
-
-    # right_side_nodes = [right_side_parent_node] + right_side_nodes_new
-
-    # right_side_edges = [
-    #     (3,4), (3,6), (3,1), (4,5), (4,6), (4,1), (4,2), (5,2), (5,6), (6,1), (6,2), (parent_tmp_node_number,1), (parent_tmp_node_number,2),
-    # ]
-
-    # right_production_side = nx.Graph()
-    # right_production_side.add_nodes_from(right_side_nodes)
-    # right_production_side.add_edges_from(right_side_edges)
 
 
 if __name__ == '__main__':
     graph = nx.Graph()
     graph.add_node(0, label='El', x=1.0, y=1.0, level=0)
 
-    draw(graph, 'before.png')
-    p1(graph)
-    draw(graph, 'after_1.png')
-    p2(graph)
-    # draw(graph, 'after_2.png')
+    draw(graph, filename='before.png')
+    p1(graph, 0)
+    draw(graph, filename='after_1.png')
+    p2(graph, 1)
+    draw(graph, filename='after_2.png')
+    # p2(graph, 1)
+    # draw(graph, filename='after_2.png')
+    # p2(graph, 1)
 
 
 # [A,B,C,D,E]
