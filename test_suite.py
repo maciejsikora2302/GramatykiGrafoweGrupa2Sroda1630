7082de0c9c5713ecb2dcd7c7cc1534c1eb25d1ea
Paweł Marszał <marszal.pawel@yahoo.com>
2022-12-12 19:23:36 +0100

    Cover P4 with unit tests

--- test_suite.py
+++ test_suite.py
@@ -126,15 +126,121 @@ class P4_Test(unittest.TestCase):
         pass
 
     def test_should_do_nothing_when_no_isomorphic_subgraph_found(self):
-        pass
+        # given
+        level = 2
 
-    def test_should_split_one_trianle_when_isomorphic_subgraph_found(self):
-        pass
+        nodes = [
+            (1, dict(label='E', x=0.0, y=1.0, level=level)),
+            (2, dict(label='E', x=1.0, y=1.0, level=level)),
+            (3, dict(label='E', x=1.0, y=0.5, level=level)),
+            (4, dict(label='E', x=1.0, y=0.0, level=level)),
+            (5, dict(label='E', x=0.0, y=0.0, level=level)),
+            (6, dict(label='I', x=1/3, y=2/3, level=level)),
+            (7, dict(label='I', x=2/3, y=1/3, level=level))
+        ]
+        edges = [
+            (1,2), (2,3), (3,4), (4,5), (5,1), # boundary edges
+            (2,5), # internal edge
+            (1,6), (2,6), (5,6), # internal I node of triangle <1,2,5>
+            (2,7), (4,7), (5,7)  # internal I node of triangle <2,4,5>
+        ]
 
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # when
+        p4(G, level)
+
+        # then
+        self.assertEqual(len(G.nodes), len(nodes))
+        self.assertEqual(len(G.edges), len(edges))
+        self.assertEqual(G.nodes[6]['label'], 'I')
+        self.assertEqual(G.nodes[7]['label'], 'I')
+
+    def test_should_split_trianle_when_isomorphic_subgraph_found(self):
+        # given
+        level = 2
+
+        nodes = [
+            (1, dict(label='E', x=0.0, y=1.0, level=level)),
+            (2, dict(label='E', x=1.0, y=1.0, level=level)),
+            (3, dict(label='E', x=1.0, y=0.5, level=level)),
+            (4, dict(label='E', x=1.0, y=0.0, level=level)),
+            (5, dict(label='E', x=0.0, y=0.0, level=level)),
+            (6, dict(label='I', x=1/3, y=2/3, level=level)),
+            (7, dict(label='I', x=2/3, y=1/3, level=level)),
+            (8, dict(label='E', x=0.5, y=0.5, level=level))
+        ]
+        edges = [
+            (1,2), (2,3), (3,4), (4,5), (5,1), # boundary edges
+            (2,8), (8,5), # internal edges
+            (1,6), (2,6), (5,6), # internal I node of triangle <1,2,5>
+            (2,7), (4,7), (5,7)  # internal I node of triangle <2,4,5>
+        ]
+
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # when
+        p4(G, level)
+
+        # then
+        self.assertEqual(len(G.nodes), len(nodes) + 8)
+        self.assertEqual(len(G.edges), len(edges) + 16 + 3)
+        self.assertEqual(G.nodes[6]['label'], 'I')
+        self.assertEqual(G.nodes[7]['label'], 'i')
+    
     def test_should_split_exactly_one_triangle_in_each_call(self):
-        pass
+        # given
+        level = 2
 
+        nodes = [
+            (1, dict(label='E', x=0.0, y=1.0, level=level)),
+            (2, dict(label='E', x=1.0, y=1.0, level=level)),
+            (3, dict(label='E', x=1.0, y=0.5, level=level)),
+            (4, dict(label='E', x=1.0, y=0.0, level=level)),
+            (5, dict(label='E', x=0.0, y=0.0, level=level)),
+            (6, dict(label='I', x=1/3, y=2/3, level=level)),
+            (7, dict(label='I', x=2/3, y=1/3, level=level)),
+            (8, dict(label='E', x=0.5, y=0.5, level=level)),
+            (9, dict(label='E', x=0.0, y=0.5, level=level))
+        ]
+        edges = [
+            (1,2), (2,3), (3,4), (4,5), (5,9), (9,1), # boundary edges
+            (2,8), (8,5), # internal edges
+            (1,6), (2,6), (5,6), # internal I node of triangle <1,2,5>
+            (2,7), (4,7), (5,7)  # internal I node of triangle <2,4,5>
+        ]
 
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # when-then: first-call
+        p4(G, level)
+
+        self.assertEqual(len(G.nodes), len(nodes) + 8)
+        self.assertEqual(len(G.edges), len(edges) + 16 + 3)
+        broken_triangles_cnt = sum([1 if G.nodes[x]['label'] == 'i' else 0 for x in [6, 7]])
+        self.assertEqual(broken_triangles_cnt, 1)
+
+        # when-then: second-call
+        p4(G, level)
+
+        self.assertEqual(len(G.nodes), len(nodes) + 8 * 2)
+        self.assertEqual(len(G.edges), len(edges) + (16 + 3) * 2)
+        self.assertEqual(G.nodes[6]['label'], 'i')
+        self.assertEqual(G.nodes[7]['label'], 'i')
+
+        # when-then: third-call (should not change)
+        p4(G, level)
+
+        self.assertEqual(len(G.nodes), len(nodes) + 8 * 2)
+        self.assertEqual(len(G.edges), len(edges) + (16 + 3) * 2)
+        self.assertEqual(G.nodes[6]['label'], 'i')
+        self.assertEqual(G.nodes[7]['label'], 'i')
 
 if __name__ == '__main__':
     unittest.main()
