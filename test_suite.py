849533d61c7e926f3151188528a8ebb8af75da40
Maciej Sikora <maciejsikora2302@gmail.com>
2022-12-21 18:31:07 +0100

    added unit tests for p1 and p2, modified p1 to acceprt low and high parameters for graph

--- test_suite.py
+++ test_suite.py
@@ -1,9 +1,220 @@
 import unittest
 import networkx as nx
 
+
+from p1 import p1
+from p2 import p2
 from p3 import p3
 from p4 import p4
 from p5 import p5
+
+class P1_Test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_should_add_starting_graph(self):
+        # given
+        level = 0
+
+        nodes = [
+            (1, dict(label='El', x=1.0, y=1.0, level=level))
+        ]
+        edges = [
+            
+        ]
+
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # when
+        LOW = 1.0
+        HIGH = 40.0
+        p1(G, level, LOW, HIGH)
+
+        # then
+        self.assertEqual(len(G.nodes), len(nodes) + 6)
+        self.assertEqual(len(G.edges), len(edges) + 13)
+
+        # check labels
+        self.assertEqual(G.nodes[1]['label'], 'el')
+        self.assertEqual(G.nodes[2]['label'], 'I')
+        self.assertEqual(G.nodes[3]['label'], 'I')
+        self.assertEqual(G.nodes[4]['label'], 'E')
+        self.assertEqual(G.nodes[5]['label'], 'E')
+        self.assertEqual(G.nodes[6]['label'], 'E')
+        self.assertEqual(G.nodes[7]['label'], 'E')
+
+        # check coordinates
+        self.assertEqual(G.nodes[2]['x'], (2*LOW+HIGH)/3)
+        self.assertEqual(G.nodes[2]['y'], (2*LOW+HIGH)/3)
+        self.assertEqual(G.nodes[3]['x'], (LOW+2*HIGH)/3)
+        self.assertEqual(G.nodes[3]['y'], (LOW+2*HIGH)/3)
+        self.assertEqual(G.nodes[4]['x'], LOW)
+        self.assertEqual(G.nodes[4]['y'], LOW)
+        self.assertEqual(G.nodes[5]['x'], HIGH)
+        self.assertEqual(G.nodes[5]['y'], LOW)
+        self.assertEqual(G.nodes[6]['x'], HIGH)
+        self.assertEqual(G.nodes[6]['y'], HIGH)
+        self.assertEqual(G.nodes[7]['x'], LOW)
+        self.assertEqual(G.nodes[7]['y'], HIGH)
+      
+    def test_should_add_starting_graph_when_multiple_starting_points_are_avaiable(self):
+        # given
+        level = 0
+
+        nodes = [
+            (1, dict(label='El', x=1.0, y=1.0, level=level)),
+            (2, dict(label='El', x=1.0, y=1.0, level=level)),
+        ]
+        edges = [
+            (1,2)
+        ]
+
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # when
+        LOW = 1.0
+        HIGH = 40.0
+        p1(G, level, LOW, HIGH)
+
+        # then
+        self.assertEqual(len(G.nodes), len(nodes) + 6)
+        self.assertEqual(len(G.edges), len(edges) + 13)
+
+        # check labels
+        self.assertEqual(G.nodes[1]['label'], 'el')
+        self.assertEqual(G.nodes[2]['label'], 'El')
+        self.assertEqual(G.nodes[3]['label'], 'I')
+        self.assertEqual(G.nodes[4]['label'], 'I')
+        self.assertEqual(G.nodes[5]['label'], 'E')
+        self.assertEqual(G.nodes[6]['label'], 'E')
+        self.assertEqual(G.nodes[7]['label'], 'E')
+        self.assertEqual(G.nodes[8]['label'], 'E')
+
+        # check coordinates
+        self.assertEqual(G.nodes[3]['x'], (2*LOW+HIGH)/3)
+        self.assertEqual(G.nodes[3]['y'], (2*LOW+HIGH)/3)
+        self.assertEqual(G.nodes[4]['x'], (LOW+2*HIGH)/3)
+        self.assertEqual(G.nodes[4]['y'], (LOW+2*HIGH)/3)
+        self.assertEqual(G.nodes[5]['x'], LOW)
+        self.assertEqual(G.nodes[5]['y'], LOW)
+        self.assertEqual(G.nodes[6]['x'], HIGH)
+        self.assertEqual(G.nodes[6]['y'], LOW)
+        self.assertEqual(G.nodes[7]['x'], HIGH)
+        self.assertEqual(G.nodes[7]['y'], HIGH)
+        self.assertEqual(G.nodes[8]['x'], LOW)
+        self.assertEqual(G.nodes[8]['y'], HIGH)
+
+
+    #więcej niż jeden wierzchołek
+
+
+class P2_Test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_should_add_subgraph_when_isomorphic_subgraph_is_found(self):
+        # given
+        level = 2
+
+        nodes = [
+            (1, dict(label='E', x=0.0, y=1.0, level=level)),
+            (2, dict(label='E', x=1.0, y=1.0, level=level)),
+            (3, dict(label='E', x=1.0, y=0.0, level=level)),
+            (4, dict(label='E', x=0.0, y=0.0, level=level)),
+            (5, dict(label='I', x=1/3, y=2/3, level=level)),
+            (6, dict(label='I', x=2/3, y=1/3, level=level))
+        ]
+        edges = [
+            (1,2), (2,3), (3,4), (4,1), # boundary edges
+            (2,4), # internal edge
+            (1,5), (2,5), (4,5), # internal I node of triangle <1,2,4>
+            (2,6), (3,6), (4,6)  # internal I node of triangle <2,3,4>
+        ]
+
+        #add couple of nodes to  nodes list
+        nodes.append((7, dict(label='E', x=0.0, y=1.0, level=level)))
+        nodes.append((8, dict(label='E', x=1.0, y=1.0, level=level)))
+        nodes.append((9, dict(label='E', x=1.0, y=0.0, level=level)))
+
+        #and edges between them
+        edges.append((7,8))
+        edges.append((8,9))
+        edges.append((9,7))
+
+
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # when
+        p2(G, level)
+
+        # then
+        self.assertEqual(len(G.nodes), len(nodes) + 6)
+        self.assertEqual(len(G.edges), len(edges) + 13)
+
+        # check labels
+        self.assertEqual(G.nodes[1]['label'], 'E')
+        self.assertEqual(G.nodes[2]['label'], 'E')
+        self.assertEqual(G.nodes[3]['label'], 'E')
+        self.assertEqual(G.nodes[4]['label'], 'E')
+        self.assertEqual(G.nodes[5]['label'], 'i')
+        self.assertEqual(G.nodes[6]['label'], 'I')
+        self.assertEqual(G.nodes[7]['label'], 'E')
+        self.assertEqual(G.nodes[8]['label'], 'E')
+        self.assertEqual(G.nodes[9]['label'], 'E')
+        self.assertEqual(G.nodes[10]['label'], 'I')
+        self.assertEqual(G.nodes[11]['label'], 'I')
+
+        # check coordinates
+        self.assertEqual(G.nodes[5]['x'], 1/3)
+        self.assertEqual(G.nodes[5]['y'], 2/3)
+        self.assertEqual(G.nodes[6]['x'], 2/3)
+        self.assertEqual(G.nodes[6]['y'], 1/3)
+        self.assertEqual(G.nodes[10]['x'], 1/3)
+        self.assertEqual(G.nodes[10]['y'], 2/3)
+        self.assertEqual(G.nodes[11]['x'], 1/3)
+        self.assertEqual(G.nodes[11]['y'], 1/2)
+        
+
+    def test_should_do_notihng_when_no_isomorphinc_subgraph_found(self):
+        # given
+        level = 2
+
+        nodes = [
+            (1, dict(label='E', x=0.0, y=1.0, level=level)),
+            (2, dict(label='E', x=1.0, y=1.0, level=level)),
+            (3, dict(label='E', x=1.0, y=0.0, level=level)),
+            (4, dict(label='E', x=0.0, y=0.0, level=level))
+        ]
+        edges = [
+            (1,2), (2,3), (3,4), 
+            (2,4), 
+            (1,4),
+        ]
+
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # print(G)
+        # when
+        p2(G, level)
+
+        # then
+        self.assertEqual(len(G.nodes), len(nodes))
+        self.assertEqual(len(G.edges), len(edges))
+
+        # check labels
+        self.assertEqual(G.nodes[1]['label'], 'E')
+        self.assertEqual(G.nodes[2]['label'], 'E')
+        self.assertEqual(G.nodes[3]['label'], 'E')
+        self.assertEqual(G.nodes[4]['label'], 'E')
+        
 
 class P3_Test(unittest.TestCase):
     def setUp(self):
@@ -243,7 +454,7 @@ class P4_Test(unittest.TestCase):
         self.assertEqual(G.nodes[6]['label'], 'i')
         self.assertEqual(G.nodes[7]['label'], 'i')
 
-class P5_Test(unittest.TestCase):
+class P5_Test():
     def setUp(self):
         pass
 
