1b5dda411b00e31b344594b16189f01703ec0261
sswrk <swierk.szymon99@gmail.com>
2023-01-04 17:29:06 +0100

    add nodes dont match tests

--- test_suite.py
+++ test_suite.py
@@ -386,6 +386,39 @@ class P5_Test(unittest.TestCase):
         self.assertEqual(G.nodes[7]['label'], 'I')
         self.assertEqual(G.nodes[9]['label'], 'I')
 
+    def test_should_do_nothing_when_node_labels_dont_match(self):
+        #given
+        level = 2
+
+        nodes = [
+            (1, dict(label='E', x=0.0, y=1.0, level=level)),
+            (2, dict(label='E', x=0.0, y=0.0, level=level)),
+            (3, dict(label='E', x=1.0, y=0.0, level=level)),
+            (4, dict(label='E', x=0.0, y=0.5, level=level)),
+            (5, dict(label='I', x=0.5, y=0.0, level=level)),
+            (6, dict(label='E', x=0.5, y=0.5, level=level)),
+            (7, dict(label='E', x=1/3, y=2/3, level=level)),
+            (8, dict(label='E', x=2/3, y=0.0, level=level)),
+            (9, dict(label='I', x=1/3, y=1/3, level=level))
+        ]
+        edges = [
+            (1,4), (4,2), (2,5), (5,3), (3,6), (6,1), # boundary edges
+            (1,8), (2,8), # internal edges
+            (1,7), (2,7), (3,7), # internal I node of triangle <1,2,3>
+            (1,9), (2,9), (8,9)  # internal I node of triangle <1,2,8>
+        ]
+
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # when
+        p5(G, level)
+
+        # then
+        self.assertEqual(len(G.nodes), len(nodes))
+        self.assertEqual(len(G.edges), len(edges))
+
 class P6_Test(unittest.TestCase):
     def setUp(self):
         pass
@@ -585,6 +618,67 @@ class P6_Test(unittest.TestCase):
             (9, dict(label='I', x=1/3, y=1/3, level=level+1)),
             (10, dict(label='I', x=2/3, y=2/3, level=level+1)),
             (11, dict(label='E', x=0.2, y=0.0, level=level+1)),
+            (12, dict(label='E', x=0.5, y=0.5, level=level+1)),
+            (13, dict(label='E', x=1.0, y=1.0, level=level+1)),
+            (14, dict(label='E', x=2/3, y=2/3, level=level+1)),
+            (15, dict(label='E', x=1.0, y=2/3, level=level+1)),
+            (16, dict(label='E', x=1/3, y=1.0, level=level+1)),
+        ]
+
+        edges = [
+            (0, 2),
+            (0, 3),
+            (1, 2),
+            (1, 3),
+            (0, 4),
+            (0, 5),
+            (1, 9),
+            (1, 10),
+            (4, 6),
+            (4, 7),
+            (5, 7),
+            (5, 8),
+            (6, 7),
+            (7, 8),
+            (9, 11),
+            (9, 12),
+            (10, 12),
+            (10, 13),
+            (11, 12),
+            (12, 13),
+            (13, 14),
+            (14, 15),
+            (15, 16),
+            (16, 14)
+        ]
+
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+
+        # when
+        p6(G, level)
+
+        # then
+        self.assertEqual(len(G.nodes), len(nodes))
+        self.assertEqual(len(G.edges), len(edges))
+
+    def test_should_merge_nodes_when_node_labels_dont_match(self):
+        # given
+        level = 2
+        nodes = [
+            (0, dict(label='i', x=0.5, y=0.5, level=level)),
+            (1, dict(label='i', x=1.0, y=0.0, level=level)),
+            (2, dict(label='E', x=0.0, y=0.0, level=level)),
+            (3, dict(label='E', x=1.0, y=1.0, level=level)),
+            (4, dict(label='I', x=1/3, y=1/3, level=level+1)),
+            (5, dict(label='I', x=2/3, y=2/3, level=level+1)),
+            (6, dict(label='E', x=0.0, y=0.0, level=level+1)),
+            (7, dict(label='E', x=0.5, y=0.5, level=level+1)),
+            (8, dict(label='I', x=1.0, y=1.0, level=level+1)),
+            (9, dict(label='E', x=1/3, y=1/3, level=level+1)),
+            (10, dict(label='I', x=2/3, y=2/3, level=level+1)),
+            (11, dict(label='E', x=0.0, y=0.0, level=level+1)),
             (12, dict(label='E', x=0.5, y=0.5, level=level+1)),
             (13, dict(label='E', x=1.0, y=1.0, level=level+1)),
             (14, dict(label='E', x=2/3, y=2/3, level=level+1)),
