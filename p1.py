4ce53139731ac7acc5d932197dfc74b6013f9e6f
Lint Action <lint-action@samuelmeuli.com>
2023-01-11 16:47:23 +0000

    Fix code style issues with Black

--- p1.py
+++ p1.py
@@ -1,37 +1,59 @@
 import networkx as nx
 from graph_functions import *
 
+
 def p1(graph: nx.Graph, level: int, low: float = 1.0, high: float = 40.0):
     left_production_side_graph = nx.Graph()
     parent_tmp_node_number = 8
 
-    left_production_side_graph.add_nodes_from([
-        (parent_tmp_node_number, dict(label='El', level=0))
-    ])
+    left_production_side_graph.add_nodes_from(
+        [(parent_tmp_node_number, dict(label="El", level=0))]
+    )
 
-    right_side_parent_node = (parent_tmp_node_number, dict(label='el', x=None, y=None, level=level))
+    right_side_parent_node = (
+        parent_tmp_node_number,
+        dict(label="el", x=None, y=None, level=level),
+    )
     right_side_nodes_new = [
-        (1, dict(label='I', x=(2*low+high)/3, y=(2*low+high)/3, level=1)),
-        (2, dict(label='I', x=(low+2*high)/3, y=(low+2*high)/3, level=1)),
-        (3, dict(label='E', x=low, y=low, level=1)),
-        (4, dict(label='E', x=high, y=low, level=1)),
-        (5, dict(label='E', x=high, y=high, level=1)),
-        (6, dict(label='E', x=low, y=high, level=1))
+        (1, dict(label="I", x=(2 * low + high) / 3, y=(2 * low + high) / 3, level=1)),
+        (2, dict(label="I", x=(low + 2 * high) / 3, y=(low + 2 * high) / 3, level=1)),
+        (3, dict(label="E", x=low, y=low, level=1)),
+        (4, dict(label="E", x=high, y=low, level=1)),
+        (5, dict(label="E", x=high, y=high, level=1)),
+        (6, dict(label="E", x=low, y=high, level=1)),
     ]
 
     right_side_nodes = [right_side_parent_node] + right_side_nodes_new
 
     right_side_edges = [
-        (3,4), (3,6), (3,1), (4,5), (4,6), (4,1), (4,2), (5,2), (5,6), (6,1), (6,2),
+        (3, 4),
+        (3, 6),
+        (3, 1),
+        (4, 5),
+        (4, 6),
+        (4, 1),
+        (4, 2),
+        (5, 2),
+        (5, 6),
+        (6, 1),
+        (6, 2),
     ]
 
     right_production_side = nx.Graph()
     right_production_side.add_nodes_from(right_side_nodes)
     right_production_side.add_edges_from(right_side_edges)
 
-    isomorphic_mapping = find_isomorphic_wrapper(graph, left_production_side_graph, level)
+    isomorphic_mapping = find_isomorphic_wrapper(
+        graph, left_production_side_graph, level
+    )
 
     if not isomorphic_mapping:
         return
-    
-    add_to_graph(graph, isomorphic_mapping, right_side_parent_node, right_side_nodes_new, right_side_edges)
+
+    add_to_graph(
+        graph,
+        isomorphic_mapping,
+        right_side_parent_node,
+        right_side_nodes_new,
+        right_side_edges,
+    )
--- p6.py
+++ p6.py
@@ -1,58 +1,65 @@
 import networkx as nx
 from graph_functions import *
 
+
 def p6(graph: nx.Graph, level: int) -> None:
     left_side_graph = nx.Graph()
 
-    left_side_graph.add_nodes_from([
-        (0, dict(label='i')),
-        (1, dict(label='i')),
-        (2, dict(label='E')),
-        (3, dict(label='E')),
-        (4, dict(label='I', level=1)),
-        (5, dict(label='I', level=1)),
-        (6, dict(label='E', level=1)),
-        (7, dict(label='E', level=1)),
-        (8, dict(label='E', level=1)),
-        (9, dict(label='I', level=1)),
-        (10, dict(label='I', level=1)),
-        (11, dict(label='E', level=1)),
-        (12, dict(label='E', level=1)),
-        (13, dict(label='E', level=1)),
-    ])
+    left_side_graph.add_nodes_from(
+        [
+            (0, dict(label="i")),
+            (1, dict(label="i")),
+            (2, dict(label="E")),
+            (3, dict(label="E")),
+            (4, dict(label="I", level=1)),
+            (5, dict(label="I", level=1)),
+            (6, dict(label="E", level=1)),
+            (7, dict(label="E", level=1)),
+            (8, dict(label="E", level=1)),
+            (9, dict(label="I", level=1)),
+            (10, dict(label="I", level=1)),
+            (11, dict(label="E", level=1)),
+            (12, dict(label="E", level=1)),
+            (13, dict(label="E", level=1)),
+        ]
+    )
 
-    left_side_graph.add_edges_from([
-        (0, 2),
-        (0, 3),
-        (1, 2),
-        (1, 3),
-        (0, 4),
-        (0, 5),
-        (1, 9),
-        (1, 10),
-        (4, 6),
-        (4, 7),
-        (5, 7),
-        (5, 8),
-        (6, 7),
-        (7, 8),
-        (9, 11),
-        (9, 12),
-        (10, 12),
-        (10, 13),
-        (11, 12),
-        (12, 13),
-    ])
+    left_side_graph.add_edges_from(
+        [
+            (0, 2),
+            (0, 3),
+            (1, 2),
+            (1, 3),
+            (0, 4),
+            (0, 5),
+            (1, 9),
+            (1, 10),
+            (4, 6),
+            (4, 7),
+            (5, 7),
+            (5, 8),
+            (6, 7),
+            (7, 8),
+            (9, 11),
+            (9, 12),
+            (10, 12),
+            (10, 13),
+            (11, 12),
+            (12, 13),
+        ]
+    )
 
     constraints = [
-        {'first_node': 6, 'second_node': 8, 'constrained_middle_node': 7},
-        {'first_node': 11, 'second_node': 13, 'constrained_middle_node': 12},
-        {'node': 6, 'constrained_equal_node': 11},
-        {'node': 7, 'constrained_equal_node': 12},
-        {'node': 8, 'constrained_equal_node': 13},
+        {"first_node": 6, "second_node": 8, "constrained_middle_node": 7},
+        {"first_node": 11, "second_node": 13, "constrained_middle_node": 12},
+        {"node": 6, "constrained_equal_node": 11},
+        {"node": 7, "constrained_equal_node": 12},
+        {"node": 8, "constrained_equal_node": 13},
     ]
 
-    isomorphic_mapping = find_isomorphic_wrapper(graph, left_side_graph, level=level, constraints=constraints)
+    isomorphic_mapping = find_isomorphic_wrapper(
+        graph, left_side_graph, level=level, constraints=constraints
+    )
 
     if not isomorphic_mapping:
         return
@@ -65,6 +72,18 @@ def p6(graph: nx.Graph, level: int) -> None:
         Y[t_node - 1] = graph.nodes[g_node][Attribute.Y]
         levels[t_node - 1] = graph.nodes[g_node][Attribute.LEVEL]
 
-    merge_nodes(graph, [isomorphic_mapping[6], isomorphic_mapping[11]], (isomorphic_mapping[6], dict(label='E', x=X[6], y=Y[6], level=levels[6])))
-    merge_nodes(graph, [isomorphic_mapping[7], isomorphic_mapping[12]], (isomorphic_mapping[7], dict(label='E', x=X[7], y=Y[7], level=levels[7])))
-    merge_nodes(graph, [isomorphic_mapping[8], isomorphic_mapping[13]], (isomorphic_mapping[8], dict(label='E', x=X[8], y=Y[8], level=levels[8])))
+    merge_nodes(
+        graph,
+        [isomorphic_mapping[6], isomorphic_mapping[11]],
+        (isomorphic_mapping[6], dict(label="E", x=X[6], y=Y[6], level=levels[6])),
+    )
+    merge_nodes(
+        graph,
+        [isomorphic_mapping[7], isomorphic_mapping[12]],
+        (isomorphic_mapping[7], dict(label="E", x=X[7], y=Y[7], level=levels[7])),
+    )
+    merge_nodes(
+        graph,
+        [isomorphic_mapping[8], isomorphic_mapping[13]],
+        (isomorphic_mapping[8], dict(label="E", x=X[8], y=Y[8], level=levels[8])),
+    )
