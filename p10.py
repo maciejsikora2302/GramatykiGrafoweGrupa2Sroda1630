ccef669855d431449d14a16cedd63668dde78790
Lint Action <lint-action@samuelmeuli.com>
2023-01-16 22:57:18 +0000

    Fix code style issues with Black

--- p10.py
+++ p10.py
@@ -6,50 +6,56 @@ def p10(graph: nx.Graph, level: int) -> None:
     left_side_graph = nx.Graph()
 
     # parent I node (from the upper level) idx = 0
-    left_side_graph.add_nodes_from([
-        (0, dict(label='i')),
-        (1, dict(label='i')),
-        (2, dict(label='E')),
-        (3, dict(label='E')),
-        (4, dict(label='I', level=1)),
-        (5, dict(label='I', level=1)),
-        (6, dict(label='I', level=1)),
-        (7, dict(label='E', level=1)),
-        (8, dict(label='E', level=1)),
-        (9, dict(label='E', level=1)),
-        (10, dict(label='E', level=1)),
-        (11, dict(label='E', level=1))
-    ])
-    left_side_graph.add_edges_from([
-        (0, 2),
-        (0, 3),
-        (1, 2),
-        (1, 3),
-        (2, 3),
-        (0, 4),
-        (0, 5),
-        (1, 6),
-        (4, 7),
-        (4, 8),
-        (5, 8),
-        (5, 9),
-        (6, 10),
-        (6, 11),
-        (7, 8),
-        (8, 9),
-        (10, 11),
-    ])
+    left_side_graph.add_nodes_from(
+        [
+            (0, dict(label="i")),
+            (1, dict(label="i")),
+            (2, dict(label="E")),
+            (3, dict(label="E")),
+            (4, dict(label="I", level=1)),
+            (5, dict(label="I", level=1)),
+            (6, dict(label="I", level=1)),
+            (7, dict(label="E", level=1)),
+            (8, dict(label="E", level=1)),
+            (9, dict(label="E", level=1)),
+            (10, dict(label="E", level=1)),
+            (11, dict(label="E", level=1)),
+        ]
+    )
+    left_side_graph.add_edges_from(
+        [
+            (0, 2),
+            (0, 3),
+            (1, 2),
+            (1, 3),
+            (2, 3),
+            (0, 4),
+            (0, 5),
+            (1, 6),
+            (4, 7),
+            (4, 8),
+            (5, 8),
+            (5, 9),
+            (6, 10),
+            (6, 11),
+            (7, 8),
+            (8, 9),
+            (10, 11),
+        ]
+    )
 
     constraints = [
-        {'first_node': 7, 'second_node': 9, 'constrained_middle_node': 8},
-        {'node': 2, 'constrained_equal_node': 7},
-        {'node': 2, 'constrained_equal_node': 10},
-        {'node': 3, 'constrained_equal_node': 9},
-        {'node': 3, 'constrained_equal_node': 11},
+        {"first_node": 7, "second_node": 9, "constrained_middle_node": 8},
+        {"node": 2, "constrained_equal_node": 7},
+        {"node": 2, "constrained_equal_node": 10},
+        {"node": 3, "constrained_equal_node": 9},
+        {"node": 3, "constrained_equal_node": 11},
     ]
 
     try:
-        isomorphic_mapping = find_isomorphic_wrapper(graph, left_side_graph, level=level, constraints=constraints)
+        isomorphic_mapping = find_isomorphic_wrapper(
+            graph, left_side_graph, level=level, constraints=constraints
+        )
     except Exception:
         return
 
@@ -62,12 +68,14 @@ def p10(graph: nx.Graph, level: int) -> None:
         levels[t_node - 1] = graph.nodes[g_node][Attribute.LEVEL]
 
     merge_nodes(
-        graph, [isomorphic_mapping[7], isomorphic_mapping[10]],
-        (isomorphic_mapping[7], dict(label='E', x=X[7], y=Y[7], level=levels[7]))
+        graph,
+        [isomorphic_mapping[7], isomorphic_mapping[10]],
+        (isomorphic_mapping[7], dict(label="E", x=X[7], y=Y[7], level=levels[7])),
     )
     merge_nodes(
-        graph, [isomorphic_mapping[9], isomorphic_mapping[11]],
-        (isomorphic_mapping[9], dict(label='E', x=X[9], y=Y[9], level=levels[9]))
+        graph,
+        [isomorphic_mapping[9], isomorphic_mapping[11]],
+        (isomorphic_mapping[9], dict(label="E", x=X[9], y=Y[9], level=levels[9])),
     )
 
     graph.remove_edge(isomorphic_mapping[7], isomorphic_mapping[9])
